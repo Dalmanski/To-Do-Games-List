fab49373acf75d7e7a5e3a1f605d0a1841047c39
Dalmanski <jayraldjohndalman@gmail.com>
2025-07-05 09:26:34 +0800

    modified:   AutoUpdate.bat 	renamed:    ToDo_Games_List.py -> To-Do Games List.py 	new file:   To-Do Games List.spec 	renamed:    build/ToDo_Games_List/Analysis-00.toc -> build/To-Do Games List/Analysis-00.toc 	renamed:    build/ToDo_Games_List/EXE-00.toc -> build/To-Do Games List/EXE-00.toc 	renamed:    build/ToDo_Games_List/PKG-00.toc -> build/To-Do Games List/PKG-00.toc 	renamed:    build/ToDo_Games_List/PYZ-00.pyz -> build/To-Do Games List/PYZ-00.pyz 	renamed:    build/ToDo_Games_List/PYZ-00.toc -> build/To-Do Games List/PYZ-00.toc 	renamed:    build/ToDo_Games_List/ToDo_Games_List.pkg -> build/To-Do Games List/To-Do Games List.pkg 	renamed:    build/ToDo_Games_List/base_library.zip -> build/To-Do Games List/base_library.zip 	renamed:    build/ToDo_Games_List/localpycs/pyimod01_archive.pyc -> build/To-Do Games List/localpycs/pyimod01_archive.pyc 	renamed:    build/ToDo_Games_List/localpycs/pyimod02_importers.pyc -> build/To-Do Games List/localpycs/pyimod02_importers.pyc 	renamed:    build/ToDo_Games_List/localpycs/pyimod03_ctypes.pyc -> build/To-Do Games List/localpycs/pyimod03_ctypes.pyc 	renamed:    build/ToDo_Games_List/localpycs/pyimod04_pywin32.pyc -> build/To-Do Games List/localpycs/pyimod04_pywin32.pyc 	renamed:    build/ToDo_Games_List/localpycs/struct.pyc -> build/To-Do Games List/localpycs/struct.pyc 	renamed:    build/ToDo_Games_List/warn-ToDo_Games_List.txt -> build/To-Do Games List/warn-To-Do Games List.txt 	renamed:    build/ToDo_Games_List/xref-ToDo_Games_List.html -> build/To-Do Games List/xref-To-Do Games List.html 	renamed:    dist/ToDo_Games_List.exe -> dist/To-Do Games List.exe

--- AutoUpdate.bat
+++ AutoUpdate.bat
@@ -1,3 +1,3 @@
 @echo off
 cd "C:\Users\Jayrald John\OneDrive\Documents\Code\Python tkinter\To-Do Games List"
-pyinstaller --noconsole --onefile --icon=icon.ico ToDo_Games_List.py
+pyinstaller --noconsole --onefile --icon=icon.ico "To-Do Games List.py"
