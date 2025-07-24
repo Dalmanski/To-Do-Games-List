96340413b97c44f7aafb68399b70a4b1ade0be6a
Dalmanski <jayraldjohndalman@gmail.com>
2025-07-24 08:38:22 +0800

    renamed:    src/AutoUpdate.bat -> AutoUpdate.bat renamed:    src/TXT_Editor.py -> TXT_Editor.py renamed:    src/To-Do Games List.py -> To-Do Games List.py renamed:    src/To-Do Games List.spec -> To-Do Games List.spec renamed:    src/To-Do Games List.txt -> To-Do Games List.txt modified:   build/To-Do Games List/Analysis-00.toc modified:   build/To-Do Games List/EXE-00.toc modified:   build/To-Do Games List/PKG-00.toc modified:   build/To-Do Games List/PYZ-00.toc modified:   build/To-Do Games List/To-Do Games List.pkg modified:   build/To-Do Games List/warn-To-Do Games List.txt modified:   dist/To-Do Games List.exe renamed:    src/icon.ico -> icon.ico renamed:    src/icon.png -> icon.png new file:   settings.json deleted:    src/ToDo_Games_List.spec deleted:    src/__pycache__/TXT_Editor.cpython-312.pyc deleted:    src/build/To-Do Games List/Analysis-00.toc deleted:    src/build/To-Do Games List/EXE-00.toc deleted:    src/build/To-Do Games List/PKG-00.toc deleted:    src/build/To-Do Games List/PYZ-00.pyz deleted:    src/build/To-Do Games List/PYZ-00.toc deleted:    src/build/To-Do Games List/To-Do Games List.pkg deleted:    src/build/To-Do Games List/base_library.zip deleted:    src/build/To-Do Games List/localpycs/pyimod01_archive.pyc deleted:    src/build/To-Do Games List/localpycs/pyimod02_importers.pyc deleted:    src/build/To-Do Games List/localpycs/pyimod03_ctypes.pyc deleted:    src/build/To-Do Games List/localpycs/pyimod04_pywin32.pyc deleted:    src/build/To-Do Games List/localpycs/struct.pyc deleted:    src/build/To-Do Games List/warn-To-Do Games List.txt deleted:    src/build/To-Do Games List/xref-To-Do Games List.html deleted:    src/dist/To-Do Games List.exe deleted:    src/settings.json renamed:    src/test.txt -> test.txt

---
+++ AutoUpdate.bat
@@ -0,0 +1,3 @@
+@echo off
+cd "%USERPROFILE%\OneDrive\Documents\Code\Python tkinter\To-Do Games List"
+pyinstaller --noconsole --onefile --icon=icon.ico "To-Do Games List.py"
--- src/AutoUpdate.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd "%USERPROFILE%\OneDrive\Documents\Code\Python tkinter\To-Do Games List\src"
-pyinstaller --noconsole --onefile --icon=icon.ico "To-Do Games List.py"
